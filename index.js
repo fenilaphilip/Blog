25e8250b5da7711e4b23788ab96683b4c7ad4d66
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2024-01-27 20:49:11 +0100

    post list - listed using ejs

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ import bodyParser from "body-parser";
 
 const port = 3000;
 const app = express(); 
+const postList = ["Last posted item","Third posted item","Second posted item","First posted item"];
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/home",(req,res)=>{
-    res.render("index.ejs");
+    res.render("index.ejs",{allPost:postList});
 });
 
 app.get("/create",(req,res)=>{
