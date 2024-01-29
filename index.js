60ffc2c616eddd767afedaf2520b18fd9ff8fe4d
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2024-01-29 11:40:23 +0100

    Button in create page is working

--- index.js
+++ index.js
@@ -2,25 +2,34 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const port = 3000;
-const app = express(); 
-const postList = ["Last posted item","Third posted item","Second posted item","First posted item"];
+const app = express();
+var postList = [];
+
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/home",(req,res)=>{
-    res.render("index.ejs",{allPost:postList});
+app.post("/create", (req, res) => {
+   postList.push(req.body);
+    console.log(postList);
+    res.redirect("/home")
+   
 });
 
-app.get("/create",(req,res)=>{
+
+app.get("/home", (req, res) => {
+    res.render("index.ejs", { allPost: postList});
+});
+
+app.get("/create", (req, res) => {
     res.render("create.ejs");
 });
 
 
-app.get("/view",(req,res)=>{
+app.get("/view", (req, res) => {
     res.render("view.ejs");
 });
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
